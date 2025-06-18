b6cd8c7cc305e04b3bc7f6048b91c69cc4db974a
Himantha Weerasingha <e18383@eng.pdn.ac.lk>
2025-06-18 21:21:24 +0530

    add app.py file

---
+++ app.py
@@ -0,0 +1,44 @@
+
+# Project: Introvert or Extrovert Prediction
+# 
+# Task: APP
+# 
+# Candidate: Himantha Weerasingha
+
+
+# Import Libraries
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Load the pre-trained model
+model = joblib.load('best_model.pkl')
+
+# Define the API endpoint '/predict' that handles POST requests
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    # Get JSON data from the incoming request
+    data = request.get_json()
+    
+    # Convert the JSON into a pandas DataFrame
+    df = pd.DataFrame([data])
+    
+    #Encode the dataframe
+    df['Stage_fear'] = df['Stage_fear'].map({'Yes': 1, 'No': 0})
+    df['Drained_after_socializing'] = df['Drained_after_socializing'].map({'Yes':1, 'No':0})
+
+    # Make a prediction
+    prediction = model.predict(df)[0]
+    
+    # Convert numeric prediction to readable result
+    result = "Introvert" if prediction == 0 else "Extrovert"
+    
+    # Return the result as a JSON response
+    return jsonify({'prediction': result})
+
+
